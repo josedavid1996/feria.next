834fbc3525fa865fb8b3717bdf7b802392db6466
Jose david Redondo <redondoperdomojosedavid@gmail.com>
2022-09-16 15:27:17 -0500

    prueba de optimizacion v44

--- src/pages/exposicion/patrocinador/[slug].jsx
+++ src/pages/exposicion/patrocinador/[slug].jsx
@@ -157,7 +157,7 @@ const Patrocinador = (props) => {
 
       <div>
         {appRendered && (
-          <Scene loading-screen="dotsColor: red">
+          <Scene Loading-screen="#sky2">
             <a-assets>
               <img
                 onLoad={() => setIsLoader(false)}
